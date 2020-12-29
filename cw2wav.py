4ef891df6a217cb7b3109cffa8f3206fd9747349
warteschleife <mthi@posteo.de>
2020-12-29 09:58:05 +0100

    Replaced cw2wav by cw2wav

--- cw2wav.py
+++ cw2wav.py
@@ -1,54 +1,67 @@
-import wave, math, random, winsound, struct, sys
+import wave, math, random, winsound, struct, sys, re
+
+
+def get_morse_table(file_name):
+    result = {}
+    with open("alphabet.txt", "r") as morse_table:
+        for line in morse_table.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith("#"):
+                continue
+            elements = re.match("^(\\S)\\s+([\\.-]+)$", line)
+            if elements:
+                if elements.group(1) in result.keys():
+                    raise Exception("Duplicate definition for character '" +
+                                    elements.group(1) + "'")
+                result[elements.group(1)] = elements.group(2)
+            else:
+                raise Exception("Invalid line in '" + file_name + "': \"" +
+                                line + "\"")
+    return result
 
 
 class CwGen:
-    def __init__(self):
-        self._dit_ms = 0.1
-        self._dah_ms = 3 * self._dit_ms
-        self._ramp_ms = (self._dit_ms / 5)
+    def __init__(self, alphabet, sampling_rate=44000, len_dit=0.1):
+        len_dit = len_dit
+        len_dah = 3 * len_dit
+        ramp_time = len_dit / 8
 
-        self._alphabet = {
-            'a': ".-",
-            'b': "-...",
-            'c': "-.-.",
-            'd': "-..",
-            'e': ".",
-            'f': "..-.",
-            'g': "--.",
-            'h': "....",
-            'i': "..",
-            'j': ".---",
-            'k': "-.-",
-            'l': ".-..",
-            'm': "--",
-            'n': "-.",
-            'o': "---",
-            'p': ".--.",
-            'q': "--.-",
-            'r': ".-.",
-            's': "...",
-            't': "-",
-            'u': "..-",
-            'v': "...-",
-            'w': ".--",
-            'x': "-..-",
-            'y': "-.--",
-            'z': "--..",
-            '0': "-----",
-            '1': ".----",
-            '2': "..---",
-            '3': "...--",
-            '4': "....-",
-            '5': ".....",
-            '6': "-....",
-            '7': "--...",
-            '8': "---..",
-            '9': "----.",
-            '?': "..--..",
-            ',': "--..--",
-            '.': ".-.-.-",
-            '=': "-...-"
-        }
+        self._sampling_rate = sampling_rate
+        self._tone_dit = self._generate_tone(len_dit, 100, ramp_time)
+        self._tone_dah = self._generate_tone(len_dah, 100, ramp_time * 3)
+        self._separate_tone = self._generate_tone(len_dit, 0, 0)
+        self._separate_char = self._generate_tone(len_dah, 0, 0)
+
+        self._alphabet = alphabet
+
+    def _generate_tone(self, duration, volume, ramp_time):
+        samples = bytearray()
+
+        periods_per_second = 680
+
+        samples_per_period = self._sampling_rate / periods_per_second
+
+        num_samples = int(self._sampling_rate * duration)
+
+        num_ramp_samples = int(self._sampling_rate * ramp_time)
+
+        for n in range(num_samples):
+            angle = (n / samples_per_period) * 2 * math.pi
+
+            if n < num_ramp_samples:
+                modulation = n / num_ramp_samples
+            elif n > (num_samples - num_ramp_samples):
+                modulation = (num_samples - n) / num_ramp_samples
+            else:
+                modulation = 1
+
+            value = int((math.sin(angle) * volume * modulation) + 127)
+
+            samples.append(value)
+
+        return samples
 
     def _simplyfy(self, input_text):
         simplified = input_text.lower()
@@ -69,72 +82,54 @@ class CwGen:
                 cw_sequence = cw_sequence + " "
                 cw_sequence = cw_sequence + " "
             else:
-                cw_sequence = cw_sequence + self._alphabet[t.lower()]
+                if not t in self._alphabet.keys():
+                    raise Exception("Character '" + t +
+                                    "' is missing in morse table.")
+
+                cw_sequence = cw_sequence + self._alphabet[t]
                 cw_sequence = cw_sequence + " "
         return cw_sequence
 
-    def _create_time_sequence(self, cw_sequence):
-        duration_seconds = 0
-
-        sequence = []
+    def _create_sample_sequence(self, cw_sequence):
+        sequence = bytearray()
 
         for t in cw_sequence:
             if t == ".":
-                up = duration_seconds
-                duration_seconds = duration_seconds + self._dit_ms
-                sequence.append((up, duration_seconds))
-                duration_seconds = duration_seconds + self._dit_ms
+                sequence = sequence + self._tone_dit
+                sequence = sequence + self._separate_tone
             elif t == "-":
-                up = duration_seconds
-                duration_seconds = duration_seconds + self._dah_ms
-                sequence.append((up, duration_seconds))
-                duration_seconds = duration_seconds + self._dit_ms
+                sequence = sequence + self._tone_dah
+                sequence = sequence + self._separate_tone
             elif t == " ":
-                duration_seconds = duration_seconds + self._dah_ms - self._dit_ms
+                sequence = sequence + self._separate_char
 
         return sequence
 
     def _write_wav_file(self, file_name, sequence):
-        duration_seconds = int(sequence[-1][1] + (2 * self._dah_ms))
-
-        print("The CW text is " + str(duration_seconds) + " seconds long.")
-
-        periods_per_second = 680
-        samples_per_second = 8000
-        samples_per_period = samples_per_second / periods_per_second
-        amplification = 100
 
         with wave.open(file_name, "wb") as w:
-            w.setnframes(samples_per_second * duration_seconds)
+            w.setnframes(len(sequence))
             w.setnchannels(1)
             w.setsampwidth(1)
-            w.setframerate(samples_per_second)
+            w.setframerate(self._sampling_rate)
 
-            for n in range(samples_per_second * duration_seconds):
-                angle = (n / samples_per_period) * 2 * math.pi
-                time = n / samples_per_second
-                value = amplification
-                if len(sequence) > 0:
-                    if time > sequence[0][0] and time < sequence[0][1]:
-                        modulation = amplification
+            w.writeframes(bytes(sequence))
 
-                        if time < sequence[0][0] + self._ramp_ms:
-                            modulation = amplification * (
-                                time - sequence[0][0]) / self._ramp_ms
-                        if time > sequence[0][1] - self._ramp_ms:
-                            modulation = amplification * (sequence[0][1] -
-                                                          time) / self._ramp_ms
-
-                        value = int(
-                            math.sin(angle) * modulation) + amplification
-                    if time >= sequence[0][1]:
-                        sequence = sequence[1:]
-                w.writeframes(struct.pack("B", value))
+    def _seconds2minuteAsText(self, total_time_seconds):
+        minutes = int(total_time_seconds / 60)
+        seconds = int(total_time_seconds - minutes * 60)
+        return str(minutes).zfill(2) + ":" + str(seconds).zfill(2)
 
     def generate(self, text, file_name):
         text = self._simplyfy(text)
         cw_sequence = self._create_cw_sequence(text)
-        signal_sequence = self._create_time_sequence(cw_sequence)
+        signal_sequence = self._create_sample_sequence(cw_sequence)
+        total_time = len(signal_sequence) / self._sampling_rate
+        character_count = len(text.replace(" ", ""))
+        print("Gesamtdauer:        " + self._seconds2minuteAsText(total_time))
+        print("Anzahl Zeichen:     " + str(character_count))
+        print("Woerter pro Minute: " +
+              str((character_count * 60 / total_time) / 5))
         self._write_wav_file(file_name, signal_sequence)
 
 
@@ -142,10 +137,21 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         exit()
 
-    with open(sys.argv[1], "r") as textfile:
-        text = (" ".join(textfile.readlines())).replace("\n", "=")
+    try:
+        alphabet = get_morse_table("alphabet.txt")
+    except Exception as ex:
+        print(ex)
+        exit(-1)
 
-        cw_gen = CwGen()
-        cw_gen.generate(text, sys.argv[2])
+    with open(sys.argv[1], "r") as textfile:
+        text = "vvv  " + (" ".join(textfile.readlines())).replace("\n", "=")
+
+        cw_gen = CwGen(alphabet, len_dit=0.12)
+
+        try:
+            cw_gen.generate(text, sys.argv[2])
+        except Exception as ex:
+            print(ex)
+            exit(-1)
 
         winsound.PlaySound(sys.argv[2], winsound.SND_FILENAME)
