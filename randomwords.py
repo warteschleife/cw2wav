f92765fd5ede31ceb6bd0e1467ac0393223302b2
warteschleife <mthi@posteo.de>
2020-12-30 09:31:20 +0100

    Random generator takes a parameter to define the alphabet.

--- randomwords.py
+++ randomwords.py
@@ -1,10 +1,11 @@
 import sys, random, util
 from util import morse_table
-alphabet = morse_table.get_morse_table("alphabet.txt")
+
+alphabet = morse_table.get_morse_table(sys.argv[1])
 
 characters = list(alphabet.keys())
 
-with open(sys.argv[1], "w") as random_text:
+with open(sys.argv[2], "w") as random_text:
     for _ in range(10):
         for _ in range(5):
             random_text.write(characters[random.randrange(0, len(characters))])
