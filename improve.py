c12acd9201e991bb13f9b34943139c84f524d940
warteschleife <mthi@posteo.de>
2021-01-02 12:47:56 +0100

    Starting point for cw training

---
+++ improve.py
@@ -0,0 +1,101 @@
+import sys, random, util
+from util import morse_table
+from util import compare
+
+alphabet = morse_table.get_morse_table("alphabet.txt")
+
+test_file = None
+result_file = None
+history_file = None
+
+
+def get_content(file_name):
+    try:
+        with open(file_name, "r") as file_handle:
+            return list(map(lambda x: x.strip(), file_handle.readlines()))
+    except:
+        return None
+
+
+def print_training_plan(statistic):
+    results = []
+    for x in statistic.keys():
+        results.append((x, statistic[x]))
+    results = list(sorted(results, key=lambda x: x[1]))
+    header = ""
+    count = ""
+    for r in results:
+        header = header + r[0].rjust(3)
+        count = count + str(r[1]).rjust(3)
+    print(header)
+    print(count)
+
+
+def compare_results(actual, expected):
+    if len(actual) == len(expected):
+        for index in range(len(actual)):
+            result = compare.compare_parts(actual[index], expected[index])
+            print()
+            print(expected[index])
+            print(result[0])
+            print(result[1].replace("r", " ").replace("m", " "))
+    else:
+        print("Cannot compare results.")
+
+
+test_file = get_content("random.txt")
+result_file = get_content("result.txt")
+
+if not result_file is None:
+    if len(test_file) == len(result_file):
+        line = " ".join(test_file) + " " + " ".join(result_file) + "\n"
+        with open("history.txt", "a") as history:
+            history.write(line)
+    compare_results(result_file, test_file)
+
+statistic = {}
+for k in alphabet.keys():
+    statistic[k] = 2
+
+with open("history.txt", "r") as history:
+    line = history.readline()
+    while line:
+        elements = list(map(lambda x: x.strip(), line.split(" ")))
+        for index in range(int(len(elements) / 2)):
+            expected_result = elements[index]
+            actual_result = elements[index + int(len(elements) / 2)]
+            compare_result = compare.compare_parts(actual_result,
+                                                   expected_result)
+            mistakes = compare_result[1]
+            while expected_result:
+                if mistakes[0] == "f" or mistakes[0] == "m":
+                    if expected_result[0] in statistic.keys():
+                        if statistic[expected_result[0]] < 20:
+                            statistic[expected_result[0]] = statistic[
+                                expected_result[0]] + 2
+                    else:
+                        statistic[expected_result[0]] = 2
+                else:
+                    if expected_result[0] in statistic.keys():
+                        if statistic[expected_result[0]] > 1:
+                            statistic[expected_result[0]] = statistic[
+                                expected_result[0]] - 1
+                    else:
+                        statistic[expected_result[0]] = 1
+
+                mistakes = mistakes[1:]
+                expected_result = expected_result[1:]
+        line = history.readline()
+
+characters = []
+for k in statistic.keys():
+    for _ in range(statistic[k]):
+        characters.append(k)
+
+print_training_plan(statistic)
+
+with open("random.txt", "w") as random_text:
+    for _ in range(5):
+        for _ in range(10):
+            random_text.write(characters[random.randrange(0, len(characters))])
+        random_text.write("\n")
---
+++ util/compare.py
@@ -0,0 +1,63 @@
+import unittest
+
+
+def count_chars(text):
+    result = 0
+    for t in text:
+        if t == "r":
+            result = result + 1
+    return result
+
+
+def compare_parts(text_a, text_b):
+    if len(text_a) == 0 and len(text_b) == 0:
+        return ("", "")
+
+    possible_result = []
+
+    if len(text_a) < len(text_b):
+        result = compare_parts(text_a, text_b[1:])
+        possible_result.append((" " + result[0], "m" + result[1]))
+    if len(text_a) > 0:
+        if text_a[0] == text_b[0]:
+            result = compare_parts(text_a[1:], text_b[1:])
+            possible_result.append((text_a[0] + result[0], "r" + result[1]))
+        else:
+            result = compare_parts(text_a[1:], text_b[1:])
+            possible_result.append((text_a[0] + result[0], "f" + result[1]))
+
+    result = possible_result[0]
+    points = count_chars(result[1])
+    for pr in possible_result[1:]:
+        pt = count_chars(pr[1])
+        if pt > points:
+            points = pt
+            result = pr
+    return result
+
+
+class TestCompare(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(("abcde", "rrrrr"), compare_parts("abcde", "abcde"))
+
+    def test_missing_char_1(self):
+        self.assertEqual((" bcde", "mrrrr"), compare_parts("bcde", "abcde"))
+
+    def test_missing_char_2(self):
+        self.assertEqual(("ab de", "rrmrr"), compare_parts("abde", "abcde"))
+
+    def test_missing_char_3(self):
+        self.assertEqual(("abcd ", "rrrrm"), compare_parts("abcd", "abcde"))
+
+    def test_missing_char_4(self):
+        self.assertEqual(("a cd ", "rmrrm"), compare_parts("acd", "abcde"))
+
+    def test_missing_char_5(self):
+        self.assertEqual(("     ", "mmmmm"), compare_parts("", "abcde"))
+
+    def test_invalid_char_1(self):
+        self.assertEqual(("xxcde", "ffrrr"), compare_parts("xxcde", "abcde"))
+
+
+if __name__ == "__main__":
+    unittest.main()
