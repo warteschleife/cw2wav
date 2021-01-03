802791e0890ae2f43b13ba9c861411a652cf84a8
warteschleife <mthi@posteo.de>
2021-01-03 06:39:22 +0100

    Longer words, less words.

--- randomwords.py
+++ randomwords.py
@@ -6,7 +6,7 @@ alphabet = morse_table.get_morse_table(sys.argv[1])
 characters = list(alphabet.keys())
 
 with open(sys.argv[2], "w") as random_text:
-    for _ in range(10):
-        for _ in range(5):
+    for _ in range(5):
+        for _ in range(10):
             random_text.write(characters[random.randrange(0, len(characters))])
         random_text.write("\n")
