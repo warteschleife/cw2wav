7246ab908200faff54ab88f12b0dd15af3a16399
warteschleife <mthi@posteo.de>
2021-01-10 17:57:11 +0100

    Single entry selection possible

--- util/rss.py
+++ util/rss.py
@@ -20,7 +20,7 @@ class _RssAtomHandler(sax.ContentHandler):
             self._summary_buffer = self._summary_buffer + text
 
         if self._element_stack == self._title_stack:
-            self._titel_buffer = self._title_buffer + text
+            self._title_buffer = self._title_buffer + text
 
     def startElement(self, name, attributes):
         if self._summary_stack is None:
@@ -49,14 +49,22 @@ class _RssAtomHandler(sax.ContentHandler):
         self._element_stack = self._element_stack[:-1]
 
         if name == self._closing_tag:
-            self._extracted_data.append(self._title_buffer.strip())
-            self._extracted_data.append(self._summary_buffer.strip())
+            self._extracted_data.append(
+                (self._title_buffer.strip(), self._summary_buffer.strip()))
 
-    def get_lines(self):
-        return self._extracted_data
+    def get_lines(self, entry_number=None):
+        result_list = self._extracted_data
+        if not entry_number is None:
+            number = int(int(entry_number) - 1)
+            result_list = [self._extracted_data[number]]
+        lines = []
+        for entry in result_list:
+            lines.append(entry[0])
+            lines.append(entry[1])
+        return lines
 
 
-def get_text_from_feed(url):
+def get_text_from_feed(url, entry_number=None):
     """ The function returns the content of the feed that is selected by 'url' """
 
     content = requests.get(url).content
@@ -65,4 +73,4 @@ def get_text_from_feed(url):
 
     sax.parseString(content, handler)
 
-    return "\n".join(handler.get_lines())
+    return "\n".join(handler.get_lines(entry_number))
