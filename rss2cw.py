77b018fe8d4005767d5897999e29862ba9825453
warteschleife <mthi@posteo.de>
2021-01-11 20:13:36 +0100

    Updated the readme.

--- rss2cw.py
+++ rss2cw.py
@@ -9,17 +9,20 @@ from util.morse_table import get_morse_table
 from util.rss import get_text_from_feed
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], "c:e:f:s:p:", [])
+    opts, args = getopt.getopt(sys.argv[1:], "a:c:e:f:s:p:", [])
 
     configuration_name = None
     feed_name = None
     output_filename = None
     text_filename = None
     entry_number = None
+    alphabet_file = "alphabet.txt"
 
     for option in opts:
         if option[0] == "-c":
             configuration_name = option[1]
+        if option[0] == "-a":
+            alphabet_file = option[1]
         if option[0] == "-f":
             feed_name = option[1]
         if option[0] == "-s":
@@ -42,7 +45,7 @@ if __name__ == "__main__":
     configuration = get_configuration(configuration_name)
 
     try:
-        alphabet = get_morse_table("alphabet.txt")
+        alphabet = get_morse_table(alphabet_file)
     except Exception as ex:
         print("Das Morsealphabet konnte nicht geladen werden:")
         print(ex)
