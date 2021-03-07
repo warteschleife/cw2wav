13b4af8091585219ce3abe140896750527fa0e30
warteschleife <mthi@posteo.de>
2021-03-07 17:21:11 +0100

    Refactored the code.

--- util/configuration.py
+++ util/configuration.py
@@ -4,22 +4,33 @@ import re
 
 
 def combine_configuration(settings, name):
-    combined_setup = {}
+
+    BASE_TAG = "base"
+
+    inheritance_path = []
 
     while name:
+        if name in inheritance_path:
+            raise Exception("Cyclic inheritance detected in configuration")
+        inheritance_path.append(name)
+
+        if BASE_TAG in settings[name].keys():
+            name = settings[name][BASE_TAG]
+        else:
+            name = None
+
+    combined_setup = {}
+
+    for name in inheritance_path:
         element = settings[name]
 
         for key in element.keys():
-            if key in ["name", "base"]:
+            if key == BASE_TAG:
                 continue
             if key in combined_setup.keys():
                 continue
             combined_setup[key] = element[key]
 
-        if "base" in element.keys():
-            name = element["base"]
-        else:
-            name = None
     return combined_setup
 
 
@@ -35,7 +46,7 @@ def replace_range_by_random(text):
     return text
 
 
-def get_configuration(name):
+def get_configuration(name="default"):
     settings = util.settings.load_settings("cw2wav.yaml")[0]
 
     if not name in settings.keys():
