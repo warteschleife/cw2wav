b8dbdd5983cf90c23b3c115bd9ebdb8d36d20b88
warteschleife <mthi@posteo.de>
2021-03-20 22:10:20 +0100

    Reduced to groups of 5

--- randomwords.py
+++ randomwords.py
@@ -12,6 +12,6 @@ characters = list(alphabet.keys())
 
 with open(sys.argv[1], "w") as random_text:
     for _ in range(5):
-        for _ in range(10):
+        for _ in range(5):
             random_text.write(characters[random.randrange(0, len(characters))])
         random_text.write("\n")
