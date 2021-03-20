72d04763f76cf8cca50b36d502bedcee9911619d
warteschleife <mthi@posteo.de>
2021-03-20 22:07:36 +0100

    Fixed randomwords

--- randomwords.py
+++ randomwords.py
@@ -1,11 +1,16 @@
 import sys, random, util
 from util import morse_table
+from util.configuration import get_configuration
 
-alphabet = morse_table.get_morse_table(sys.argv[1])
+configuration = get_configuration()
+
+alphabet_filename = configuration.get("cw_table")
+
+alphabet = morse_table.get_cw_table(alphabet_filename)
 
 characters = list(alphabet.keys())
 
-with open(sys.argv[2], "w") as random_text:
+with open(sys.argv[1], "w") as random_text:
     for _ in range(5):
         for _ in range(10):
             random_text.write(characters[random.randrange(0, len(characters))])
