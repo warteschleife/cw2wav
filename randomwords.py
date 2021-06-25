fe57d6bb534e8f44dd7e2032fbc262a08185c395
warteschleife <mthi@posteo.de>
2021-06-25 22:02:56 +0200

    Randomwords provides some info on usage

--- randomwords.py
+++ randomwords.py
@@ -1,17 +1,35 @@
-import sys, random, util
+import sys, random, os
 from util import morse_table
-from util.configuration import get_configuration
 
-configuration = get_configuration()
+if len(sys.argv) != 3:
+    print("python randomwords.py [alphabet-file] [output-file]")
+    exit()
 
-alphabet_filename = configuration.get("cw_table")
+alphabet_filename = sys.argv[1]
+
+output_filename = sys.argv[2]
+
+if not os.path.exists(alphabet_filename):
+    print("Alphabet file does not exist: '" + alphabet_filename + "'")
+    exit()
+
+if os.path.exists(output_filename):
+    print("File already exist: '" + output_filename + "'")
+    exit()
 
 alphabet = morse_table.get_cw_table(alphabet_filename)
 
 characters = list(filter(lambda x: len(x) == 1, alphabet.keys()))
 
-with open(sys.argv[1], "w") as random_text:
+info_string = "Random word created out of the following characters: "
+
+character_list = ", ".join(map(lambda x: "'" + x + "'", characters))
+
+print(info_string + character_list)
+
+with open(sys.argv[1], "w") as output_filename:
     for _ in range(5):
         for _ in range(5):
-            random_text.write(characters[random.randrange(0, len(characters))])
-        random_text.write("\n")
+            output_filename.write(characters[random.randrange(
+                0, len(characters))])
+        output_filename.write("\n")
